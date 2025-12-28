9e2e2efbbda12f03211023b1b200a70c72a11511
Ashiqul Islam Ayon <ashiqulislamayon28@gmail.com>
2025-12-29 00:25:04 +0600

    comment model added into the admin

--- backend/post/admin.py
+++ backend/post/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import PostModel, PostTranslation
+from .models import PostModel, PostTranslation, CommentModel
 
 admin.site.register(PostModel)
 admin.site.register(PostTranslation)
+admin.site.register(CommentModel)
