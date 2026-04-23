4c58428c37234d91b27cd51603f740a4037e8c66
Ashiqul Islam Ayon <ashiqulislamayon28@gmail.com>
2026-04-24 03:46:33 +0600

    request list api updated

--- backend/classroom/serializers.py
+++ backend/classroom/serializers.py
@@ -36,9 +36,15 @@ class JoinClassroomSerializer(serializers.ModelSerializer):
 class JoinRequestSerializer(serializers.ModelSerializer):
     user_first_name = serializers.CharField(source='user.first_name', read_only=True)
     user_last_name = serializers.CharField(source='user.last_name', read_only=True)
+    user_profile_pic = serializers.SerializerMethodField()
 
     class Meta:
         model = JoinRequest
-        fields = ["id", "user", "classroom", "status", "created_at", "user_first_name", "user_last_name"]
+        fields = ["id", "user", "classroom", "status", "created_at", "user_first_name", "user_last_name", "user_profile_pic"]
         read_only_fields = ["id", "created_at"]
+
+    def get_user_profile_pic(self, obj):
+        if obj.user.profile_pic:
+            return obj.user.profile_pic.url
+        return None
 
